082453ada9d5676c57d13a38f42f28716c9178ee
Guillaume BINET <gbin@gootz.net>
2012-06-08 16:39:24 +0000

    adapted the helper script to the new structure

--- scripts/err.sh
+++ scripts/err.sh
@@ -2,9 +2,9 @@
 
 # Call this script without arguments, it will call itself again, nohupped and forked
 if [ -z $1 ]; then
-    nohup ./err.sh start &
+    nohup scripts/err.sh start &
 elif [[ $1 == "start" ]]; then
     while [ true ]; do
-	nohup python err.py
+	nohup python scripts/err.py
     done
 fi

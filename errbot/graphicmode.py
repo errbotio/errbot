0af4d81cd69080b09c9981cce8347a56df0a10e3
Paul Labedan <plabedan@gmail.com>
2012-06-18 22:05:49 +0200

    add a standalone graphical mode to test html content

---
+++ errbot/graphicmode.py
@@ -0,0 +1,114 @@
+import logging
+import config
+import sys
+from PySide import QtCore, QtGui, QtWebKit
+
+class JIDMock():
+    domain = 'meuh'
+    resource = 'bidon'
+
+    def __init__(self, node):
+        self.node = node
+    def getNode(self):
+        return self.node
+    def bareMatch(self, whatever):
+        return False
+    def getStripped(self):
+        return self.node
+
+class MessageMock():
+    def __init__(self, body):
+        self.body = body
+    def getType(self):
+        return 'chat'
+    def getFrom(self):
+        return JIDMock(config.BOT_ADMINS[0])
+    def getProperties(self):
+        return {}
+    def getBody(self):
+        return self.body
+    def getThread(self):
+        return None
+
+class ConnectionMock(QtCore.QObject):
+    def __init__(self):
+        super(ConnectionMock, self).__init__()
+        
+    newAnswer = QtCore.Signal(str)
+  
+    def send(self, mess):
+        if hasattr(mess, 'getBody') and len(mess.getBody()) > 0 and not mess.getBody().isspace():
+            self.newAnswer.emit(mess.getBody())
+
+def patch_jabberbot():
+    from errbot import jabberbot
+
+    conn = ConnectionMock()
+    
+    def send_command(self):
+        self.callback_message(conn, MessageMock(self.input.text()))
+        self.input.clear()
+  
+    def htmlify(text):
+        import re
+        urlfinder = re.compile(r'http([^\.\s]+\.[^\.\s]*)+[^\.\s]{2,}')
+        def linkify(text):
+            def replacewithlink(matchobj):
+                url = matchobj.group(0)
+                text = unicode(url)
+                if text.startswith('http://'):
+                    text = text.replace('http://', '', 1)
+                elif text.startswith('https://'):
+                    text = text.replace('https://', '', 1)
+
+                if text.startswith('www.'):
+                    text = text.replace('www.', '', 1)
+
+                imglink = ''
+                for a in ['png', '.gif', '.jpg', '.jpeg', '.svg']:
+                    if text.endswith(a):
+                        imglink = '<br /><img src="' + url + '" />'
+                        break
+                return '<a class="comurl" href="' + url + '" target="_blank" rel="nofollow">' + text + '<img class="imglink" src="/images/linkout.png"></a>' + imglink
+                
+            return urlfinder.sub(replacewithlink, text)
+        return '<pre>' + linkify(text) + '</pre>'
+    
+    def receive_message(self, text):
+        self.buffer += htmlify(text)
+        self.output.setHtml(self.buffer)
+        self.output.page().mainFrame().scroll(0, 2**24)
+        
+    def fake_serve_forever(self):
+        self.jid = JIDMock('blah') # whatever
+        self.connect() # be sure we are "connected" before the first command
+        
+        app = QtGui.QApplication(sys.argv)
+        self.mainW = QtGui.QWidget()
+        vbox = QtGui.QVBoxLayout()
+        self.input = QtGui.QLineEdit()
+        self.output = QtWebKit.QWebView()
+        self.buffer = '<html><head><link rel="stylesheet" type="text/css" href="%s/style/style.css" /></head><body>' %\
+          (QtCore.QUrl.fromLocalFile(config.BOT_DATA_DIR).toString())
+        self.output.setHtml(self.buffer)
+        vbox.addWidget(self.output)
+        vbox.addWidget(self.input)
+        self.mainW.setLayout(vbox)
+        self.input.returnPressed.connect(self.send_command)
+        self.conn.newAnswer.connect(self.receive_message)
+        self.mainW.show()
+        app.exec_()
+
+    def fake_connect(self):
+        if not self.conn:
+            self.conn = ConnectionMock()
+            self.activate_non_started_plugins()
+            logging.info('Notifying connection to all the plugins...')
+            self.signal_connect_to_all_plugins()
+            logging.info('Plugin activation done.')
+        return self.conn
+
+    jabberbot.JabberBot.send_command = send_command
+    jabberbot.JabberBot.receive_message = receive_message
+    jabberbot.JabberBot.serve_forever = fake_serve_forever
+    jabberbot.JabberBot.connect = fake_connect
--- scripts/err.py
+++ scripts/err.py
@@ -92,13 +92,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='The main entry point of the XMPP bot err.')
     parser.add_argument('-c', '--config', default=getcwd(), help='Specify the directory where your config.py is (default: current working directory)')
     parser.add_argument('-t', '--test', action='store_true', help='put err in test mode on the console')
+    parser.add_argument('-G', '--graphic', action='store_true', help='Use graphical mode')
+
     if not ON_WINDOWS:
         option_group = parser.add_argument_group('arguments to run it as a Daemon')
         option_group.add_argument('-d', '--daemon', action='store_true', help='Detach the process from the console')
         option_group.add_argument('-p', '--pidfile', default=None, help='Specify the pid file for the daemon (default: current bot data directory)')
         option_group.add_argument('-u', '--user', default=None, help='Specify the user id you want the daemon to run under')
         option_group.add_argument('-g', '--group', default=None, help='Specify the group id you want the daemon to run under')
-
+    
     args = vars(parser.parse_args()) # create a dictionary of args
     config_path = args['config']
     # setup the environment to be able to import the config.py
@@ -132,6 +134,12 @@ if __name__ == "__main__":
         # Sets a minimal logging on the console for the critical config errors
         from errbot.testmode import patch_jabberbot
 
+        patch_jabberbot()
+
+    if args['graphic']:
+        # Sets a minimal logging on the console for the critical config errors
+        from errbot.graphicmode import patch_jabberbot
+        
         patch_jabberbot()
 
     main()

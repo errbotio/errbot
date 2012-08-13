806fa7e880323f646d35d0ece6ac92576305ca22
Guillaume BINET <gbin@gootz.net>
2012-08-13 11:12:51 +0200

    Added a command to test webhooks from chat

--- errbot/builtins/webserver.py
+++ errbot/builtins/webserver.py
@@ -1,6 +1,7 @@
 from inspect import getmembers, ismethod
 import logging
 from threading import Thread
+import urllib2
 from errbot import holder
 import simplejson
 from simplejson.decoder import JSONDecodeError
@@ -12,7 +13,16 @@ from flask import Response
 from werkzeug.serving import ThreadedWSGIServer
 from errbot.plugin_manager import get_all_active_plugin_objects
 from errbot.jabberbot import botcmd
+from errbot.bundled.exrex import generate
+
 OK = Response()
+
+TEST_REPORT = """*** Test Report
+Found the matching rule : %s
+Generated URL : %s
+Detected your post as : %s
+Status code : %i
+"""
 
 class WebView(View):
     def __init__(self, func, form_param):
@@ -101,11 +111,47 @@ class Webserver(BotPlugin):
 
     @botcmd
     def webstatus(self, mess, args):
-        return '\n'.join((rule.rule + "->" + rule.endpoint for rule in holder.flask_app.url_map.iter_rules()))
+        """
+        Gives a quick status of what is mapped in the internal webserver
+        """
+        return '\n'.join((rule.rule + " -> " + rule.endpoint for rule in holder.flask_app.url_map.iter_rules()))
 
-    #@webhook(r'/zourby/')
-    #def test(self, incoming_request):
-    #    logging.debug(type(incoming_request))
-    #    logging.debug(str(incoming_request))
-    #    return str(holder.bot.status(None, None))
+    @botcmd(split_args_with=' ')
+    def webhook_test(self, mess, args):
+        endpoint = args[0]
+        content = ' '.join(args[1:])
+        for rule in  holder.flask_app.url_map.iter_rules():
+            if endpoint == rule.endpoint:
+                with holder.flask_app.test_client() as client:
+                    logging.debug('Found the matching rule : %s' % rule.rule)
+                    generated_url = generate(rule.rule, 1).next() # generate a matching url from the pattern
+                    logging.debug('Generated URL : %s' % generated_url)
+
+                    # try to guess the content-type of what has been passed
+                    try:
+                        # try if it is plain json
+                        simplejson.loads(content)
+                        contenttype = 'application/json'
+                    except JSONDecodeError:
+                        # try if it is a form
+                        splitted = content.split('=')
+                        try:
+                            payload = '='.join(splitted[1:])
+                            simplejson.loads(urllib2.unquote(payload))
+                            contenttype = 'application/x-www-form-urlencoded'
+                        except Exception as e:
+                            contenttype = 'text/plain' # dunno what it is
+
+                    logging.debug('Detected your post as : %s' % contenttype)
+
+                    response = client.post(generated_url, data=content, content_type = contenttype)
+                    return TEST_REPORT %(rule.rule, generated_url, contenttype, response.status_code)
+        return 'Could not find endpoint %s. Check with !webstatus which endpoints are deployed' % endpoint
+
+
+    @webhook(r'/zourby/')
+    def zourby(self, incoming_request):
+        logging.debug(type(incoming_request))
+        logging.debug(str(incoming_request))
+        return str(holder.bot.status(None, None))
 

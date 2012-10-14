7bdeab4a84dca5e49121b4d5d0b093efdad23d11
Guillaume BINET <gbin@gootz.net>
2012-10-14 15:03:52 -0700

    added a unit test for issue 82

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,5 +1,7 @@
 # coding=utf-8
+from datetime import timedelta
 import unittest
+from xmpp import Message, Node
 from errbot.utils import *
 
 class TestUtils(unittest.TestCase):
@@ -12,3 +14,17 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(drawbar(5,10),u'[████████▒▒▒▒▒▒▒]')
         self.assertEqual(drawbar(0,10),u'[▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒]')
         self.assertEqual(drawbar(10,10),u'[███████████████]')
+
+    def test_XMPP_participant_jid_from_MUC_message(self):
+        # Real message from Hipchat MUC
+        msg_txt = """
+                <message xmlns="jabber:client" to="24926_143886@chat.hipchat.com/HipchatBot" type="groupchat" from="24926_err@conf.hipchat.com/Guillaume BINET">
+                   <body>test</body>
+                   <x xmlns="http://hipchat.com"><sender>24926_143884@chat.hipchat.com</sender></x>
+                </message>"""
+
+        message = Message(node = msg_txt)
+
+        self.assertEqual(str(message.getFrom()), "24926_err@conf.hipchat.com/Guillaume BINET")
+        self.assertEqual(str(get_jid_from_message(message)), "24926_143884@chat.hipchat.com")
+

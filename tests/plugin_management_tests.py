c21c37343528d3310b3ebe67c3c246528e404172
Guillaume BINET <gbin@gootz.net>
2012-12-23 09:12:14 -0800

    broken tests with hardcoded path

--- tests/plugin_management_tests.py
+++ tests/plugin_management_tests.py
@@ -6,5 +6,5 @@ from errbot.plugin_manager import check_dependencies
 class TestPluginManagement(unittest.TestCase):
     def test_check_dependencies(self):
         response = check_dependencies(os.path.dirname(__file__) + os.path.sep + 'assets')
-        self.assertEquals('You need those dependencies for /home/gbin/projects/err/tests/assets: impossible_requirement', response)
+        self.assertIn('impossible_requirement', response)
 

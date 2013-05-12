88b20df2b19cdc470f9bdfd32545991ab5d92da6
Nick Groenen <zoni@zoni.nl>
2013-05-12 13:38:56 +0200

    Add more webhooks tests

--- tests/webhooks_tests.py
+++ tests/webhooks_tests.py
@@ -1,12 +1,15 @@
-from errbot.backends.test import FullStackTest, pushMessage, popMessage
 import requests
+import os
+from errbot.backends.test import FullStackTest, pushMessage, popMessage
 from errbot import PY2
 
 
 class TestWebhooks(FullStackTest):
     @classmethod
     def setUpClass(cls, extra_test_file=None):
-        super(TestWebhooks, cls).setUpClass()
+        plugin_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'webhooks_tests'
+        super(TestWebhooks, cls).setUpClass(extra_test_file=plugin_dir)
+
         pushMessage("!config Webserver {'HOST': 'localhost', 'PORT': 3141, 'SSL':  None}")
         popMessage()
 
@@ -21,6 +24,14 @@ class TestWebhooks(FullStackTest):
         repr_response = "{u'toto': u'titui'}" if PY2 else "{'toto': 'titui'}"
         self.assertEquals(requests.post('http://localhost:3141/echo/', '{"toto": "titui"}').text, repr_response)  # yes as it takes a json and gives back the python representation as string
 
+    def test_plain_json_on_custom_url(self):
+        repr_response = "{u'toto': u'titui'}" if PY2 else "{'toto': 'titui'}"
+        self.assertEquals(requests.post('http://localhost:3141/custom_plainjson/', '{"toto": "titui"}').text, repr_response)  # yes as it takes a json and gives back the python representation as string
+
     def test_form(self):
         payload = {'toto': 'titui'}
         self.assertEquals(requests.post('http://localhost:3141/echo/', data=payload).text, "{'toto': 'titui'}")  # yes as it takes a json and gives back the python representation as string
+
+    def test_form_on_custom_url(self):
+        payload = {'toto': 'titui'}
+        self.assertEquals(requests.post('http://localhost:3141/custom_form/', data=payload).text, "{'toto': 'titui'}")  # yes as it takes a json and gives back the python representation as string

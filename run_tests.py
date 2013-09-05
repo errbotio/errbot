8388b5700c237a26d1694e44580e02c83683dc22
Nick Groenen <nick@travelbird.nl>
2013-09-05 12:04:00 +0200

    Make webtest a hard dependency
    Previously, webtest was only required when running this tests. Commit
    aaf704b6 made it a requirement for the Webserver plugin however, so it
    should be listed in requirements.txt as well.

--- run_tests.py
+++ run_tests.py
@@ -13,12 +13,6 @@ except ImportError:
     sys.stderr.write("Tests require the 'nose' package which you are currently missing.\nYou can install nose with `pip install nose`.\n")
     sys.exit(1)
 
-try:
-    import webtest
-except ImportError:
-    sys.stderr.write("Tests require the 'webtest' package which you are currently missing.\nYou can install webtest with `pip install webtest`.\n")
-    sys.exit(1)
-
 if not PY3:  # hack the path system to take the python 2 converted sources
     print('Changing root to ' + py2_root)
     print('Sys path ' + ', '.join(sys.path))

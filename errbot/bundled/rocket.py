2230c7516871c3d17a0770d44155c547223ddbe1
Nick Groenen <nick@groenen.me>
2014-07-12 16:21:59 +0200

    Update bundled copy of Rocket
    Fixes #268

--- errbot/bundled/rocket.py
+++ errbot/bundled/rocket.py
@@ -552,7 +552,11 @@ class Listener(Thread):
                 if not self.ready:
                     if __debug__:
                         self.err_log.debug('Listener exiting.')
-                    self.listener.shutdown(socket.SHUT_RDWR)
+                    try:
+                        self.listener.shutdown(socket.SHUT_RDWR)
+                    except socket.error as e:
+                        self.err_log.warning('Socket shutdown() failed, using close()')
+                        self.listener.close()
                     return
                 else:
                     continue

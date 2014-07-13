ea205750a7f90f66834a342ac289efc0209f33ef
Nick Groenen <nick@groenen.me>
2014-07-13 16:14:06 +0200

    Use pytest instead of nose in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,37 +1,12 @@
 #!/usr/bin/env python
-import sys
+
+import pytest
 import subprocess
-import os
-from glob import glob
+import sys
 
 PY3 = sys.version_info[0] == 3
-TRAVIS_INCOMPATIBLE = ('webhooks_tests.py',)
 
-# Set nose verbosity level to verbose by default
-os.environ['NOSE_VERBOSE'] = os.environ.get('NOSE_VERBOSE', "2")
-
-try:
-    import nose
-except ImportError:
-    sys.stderr.write(
-        "Tests require the 'nose' package which you are currently missing.\n"
-        "You can install nose with `pip install nose`.\n"
-    )
-    sys.exit(1)
-
-# Webhooks tests fail when run together with the other tests, but pass correctly
-# when run in isolation. We work around this issue by running each set of tests
-# separately. It's an ugly hack, but it works.
-segments = ('tests', '*.py')
-testsuites = glob(os.sep.join(segments))
-testresults = []
-
-for testsuite in testsuites:
-    if os.environ.get("TRAVIS", "False") == "true" and os.path.basename(testsuite) in TRAVIS_INCOMPATIBLE:
-        print("Incompatible test {} skipped".format(testsuite))
-        continue
-    print("\nRunning tests from {}\n".format(testsuite))
-    testresults.append(nose.run(defaultTest=testsuite))
+pytest_result = pytest.main()
 
 # Skip pep8 checks on Python 2 because the code conversion done by
 # 3to2 tends to mess things up
@@ -43,9 +18,5 @@ else:
     print("Running under Python 2, skipping pep8 style checks")
     pep8_result = 0
 
-if False in testresults or pep8_result:
-    print("\nSome tests failed to pass!")
-    exit(-99)  # a test did not pass
-else:
-    print("\nAll tests have successfully passed")
-    exit(0)  # no error
+if pytest_result or pep8_result:
+    sys.exit(1)

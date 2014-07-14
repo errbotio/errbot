5cb04dada9e63516e0780df8209118cfa36ef244
Nick Groenen <nick@groenen.me>
2014-07-14 17:40:02 +0200

    Call pep8 checker from run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
-import sys
-import pytest
 
-sys.exit(pytest.main())
+import pytest
+import subprocess
+import sys
+
+pytest_result = pytest.main()
+# py.test has a pep8 plugin, however it has considerably fewer options
+# for configuration, hence the use of the stand-alone pep8 checker instead.
+pep8_result = subprocess.call(["pep8", "--statistics", "--show-source"])
+
+if pytest_result or pep8_result:
+    sys.exit(1)

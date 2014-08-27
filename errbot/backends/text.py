e933f5a72f8b0ccd018b0b1b2a8cb57a57e80def
Guillaume BINET <gbin@gootz.net>
2014-08-27 13:15:59 -0700

    added presence to the text backend

--- errbot/backends/text.py
+++ errbot/backends/text.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import config
-from errbot.backends.base import Message, build_message, Identifier
+from errbot.backends.base import Message, build_message, Identifier, Presence, ONLINE, OFFLINE
 from errbot.errBot import ErrBot
 
 
@@ -21,20 +21,24 @@ class TextBackend(ErrBot):
 
     def serve_forever(self):
         self.jid = Identifier('Err')
+        me = Identifier(config.BOT_ADMINS[0])
         self.connect()  # be sure we are "connected" before the first command
         self.connect_callback()  # notify that the connection occured
+        self.callback_presence(self.conn, Presence(identifier=me, status=ONLINE))
         try:
             while True:
                 entry = input("Talk to  me >>")
                 msg = Message(entry)
-                msg.frm = config.BOT_ADMINS[0]  # assume this is the admin talking
-                msg.to = self.jid  # To me only
+                msg.frm = me
+                msg.to = self.jid
                 self.callback_message(self.conn, msg)
         except EOFError as eof:
             pass
         except KeyboardInterrupt as ki:
             pass
         finally:
+            # simulate some real presence
+            self.callback_presence(self.conn, Presence(identifier=me, status=OFFLINE))
             logging.debug("Trigger disconnect callback")
             self.disconnect_callback()
             logging.debug("Trigger shutdown")

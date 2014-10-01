aef572ab790e0510d247499087039b8f8bf185fd
Nick Groenen <nick@groenen.me>
2014-10-01 03:02:15 +0200

    Remove HipChat html support.
    HipChat does not support incoming html-im [0] and has only
    rudimentary support for HTML if you send it through the HTTP
    API. There are however two main drawbacks with this (as documented on
    the API page [1] for that call):
    
    * Special HipChat features such as @mentions, emoticons, and image
      previews are NOT supported when using this format.
    * There may be a slight delay before messages appear in the room.
    
    Furthermore, the API is subject to pretty strict rate limiting. Personal
    access tokens only get 100 requests per 5 minutes [2]. It would be very
    trivial to exhaust the available number of API calls, blocking the API
    from functioning and preventing further messages from going out.
    It would also prevent the MUC management from working, since it's
    subject to the same API limits.
    
    With these limitations currently in place, it is my belief that simply
    sending the xhtml-im part like with any other XMPP server, letting it
    get ignored by HipChat until such a time that they do support it (they
    mention it is a wish they have, see the official reaction in the
    comments on [0]) is better than making it sort-of-work by intercepting
    messages and routing them over to the HTTP API.
    
    [0]: http://help.hipchat.com/forums/138883-suggestions-ideas/suggestions/2974079-support-xhtml-im-send-text-only-notifications-to
    [1]: https://www.hipchat.com/docs/apiv2/method/send_room_notification
    [2]: https://www.hipchat.com/docs/apiv2/rate_limiting

--- errbot/backends/hipchat.py
+++ errbot/backends/hipchat.py
@@ -1,33 +1,4 @@
-import json
-import logging
-from urllib.parse import urlencode
-from urllib.request import urlopen, Request
-
-from config import CHATROOM_FN
 from errbot.backends.xmpp import XMPPBackend, XMPPConnection
-from errbot.utils import utf8, REMOVE_EOL, mess_2_embeddablehtml
-import re
-
-HIPCHAT_MESSAGE_URL = 'https://api.hipchat.com/v1/rooms/message'
-
-HIPCHAT_FORCE_PRE = re.compile(r'<body>', re.I)
-HIPCHAT_FORCE_SLASH_PRE = re.compile(r'</body>', re.I)
-HIPCHAT_EOLS = re.compile(r'</p>|</li>', re.I)
-HIPCHAT_BOLS = re.compile(r'<p [^>]+>|<li [^>]+>', re.I)
-
-
-def xhtml2hipchat(xhtml):
-    # Hipchat has a really limited html support
-    retarded_hipchat_html_plain = REMOVE_EOL.sub('', xhtml)  # Ignore formatting
-    # Readd the \n where they probably fit best
-    retarded_hipchat_html_plain = HIPCHAT_EOLS.sub('<br/>', retarded_hipchat_html_plain)
-    # Zap every tag left
-    retarded_hipchat_html_plain = HIPCHAT_BOLS.sub('', retarded_hipchat_html_plain)
-    # Fix pre
-    retarded_hipchat_html_plain = HIPCHAT_FORCE_PRE.sub('<body><pre>', retarded_hipchat_html_plain)
-    # Fix /pre
-    retarded_hipchat_html_plain = HIPCHAT_FORCE_SLASH_PRE.sub('</pre></body>', retarded_hipchat_html_plain)
-    return retarded_hipchat_html_plain
 
 
 class HipchatClient(XMPPConnection):
@@ -35,22 +6,6 @@ class HipchatClient(XMPPConnection):
         self.token = kwargs.pop('token')
         self.debug = kwargs.pop('debug')
         super(HipchatClient, self).__init__(*args, **kwargs)
-
-    def send_api_message(self, room_id, fr, message, message_format='html'):
-        base = {'format': 'json', 'auth_token': self.token}
-        red_data = {'room_id': room_id, 'from': fr, 'message': utf8(message), 'message_format': message_format}
-        req = Request(url=HIPCHAT_MESSAGE_URL + '?' + urlencode(base), data=urlencode(red_data))
-        return json.load(urlopen(req))
-
-    def send_message(self, mess):
-        if self.token and mess.type == 'groupchat':
-
-            logging.debug('Message intercepted for Hipchat API')
-            content, _ = mess_2_embeddablehtml(mess)
-            room_jid = mess.to
-            self.send_api_message(room_jid.node.split('_')[1], CHATROOM_FN, content)
-        else:
-            super(HipchatClient, self).send_message(mess)
 
 
 # It is just a different mode for the moment

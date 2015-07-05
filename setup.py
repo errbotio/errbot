a73d497bbaa15f162a54c37a6eae186902b7fa54
Guillaume Binet <gbin@gootz.net>
2015-07-05 14:39:19 -0700

    added 3.4 support to the metadata.

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
         ],
         src_root=src_root,
         platforms='any',

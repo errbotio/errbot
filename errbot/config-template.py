165175028a86fac546873b61b10cd3a496d32cc0
Guillaume Binet <gbin@google.com>
2015-07-22 11:05:12 -0700

    Split campfire out of the main codebase.
    It is now at https://github.com/gbin/err-backend-campfire

--- errbot/config-template.py
+++ errbot/config-template.py
@@ -32,9 +32,10 @@ import logging
 # 'Graphic'  - in a GUI window
 
 # Commercial backends:
-# 'Campfire' - see https://campfirenow.com/
+# 'Campfire' - see https://campfirenow.com/ (follow instructions from https://github.com/gbin/err-backend-campfire)
 # 'Hipchat'  - see https://www.hipchat.com/
 # 'Slack'    - see https://slack.com/
+# 'Gitter'   - see https://gitter.im/ (follow instructions from https://github.com/gbin/err-backend-gitter)
 
 # Open protocols:
 # 'TOX'      - see https://tox.im/
@@ -114,11 +115,6 @@ BOT_IDENTITY = {
     ## of HipChat then you may leave this commented out.
     # 'endpoint' : 'https://api.hipchat.com'
 
-    ## Campfire mode (Comment the others above if using this mode)
-    # 'subdomain': 'yatta',
-    # 'username' : 'errbot',
-    # 'password' : 'changeme',
-
     ## TOX Mode (comment the others above if using this mode)
     # 'username': 'errbot',
 
@@ -140,14 +136,11 @@ BOT_IDENTITY = {
 # Set the admins of your bot. Only these users will have access
 # to the admin-only commands.
 #
-# Campfire syntax is the full name:
-# BOT_ADMINS = ('Guillaume Binet',)
-#
 # TOX syntax is a hash.
 # BOT_ADMINS = ['F9886B47503FB80E6347CC0907D8000144305796DE54693253AA5E574E5E8106C7D002557189', ]
 BOT_ADMINS = ('gbin@localhost',)
 
-# Chatrooms your bot should join on startup. For the IRC backend you 
+# Chatrooms your bot should join on startup. For the IRC backend you
 # should include the # sign here. For XMPP rooms that are password
 # protected, you can specify another tuple here instead of a string,
 # using the format (RoomName, Password).

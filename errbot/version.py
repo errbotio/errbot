3eef6a5e29292f035d69c2d63f501ac20a075101
Guillaume Binet <gbin@google.com>
2015-09-12 11:00:08 -0700

    version

--- errbot/version.py
+++ errbot/version.py
@@ -1,7 +1,3 @@
 # just the current version of err
 # it is used for deployment on pypi AND for version checking at plugin load time
-<<<<<<< HEAD
 VERSION = '3.1.0'
-=======
-VERSION = '3.0.3'
->>>>>>> 3.0

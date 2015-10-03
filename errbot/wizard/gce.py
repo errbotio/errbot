6feaa9cf0c115553f1574777e8f901cb56beee4d
Guillaume Binet <gbin@google.com>
2015-10-03 16:34:54 -0700

    cleanup / code conv.

--- errbot/wizard/gce.py
+++ errbot/wizard/gce.py
@@ -1,47 +1,54 @@
+# vim: ts=4:sw=4
 import webbrowser
-import httplib2
 import os
+import sys
 from urllib.parse import parse_qsl
 from oauth2client import client
 from googleapiclient.discovery import build
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class AuthHandler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header("Content-type", "text/html")
-    self.end_headers()
-    query = self.path.split('?', 1)[-1]
-    query = dict(parse_qsl(query))
-    if 'error' in query:
-      sys.exit('Authentication request was rejected.')
-    if 'code' in query:
-      global code
-      code = query['code']
-    self.server.query_params = query
-    self.wfile.write(b"<html><head><title>Authentication Status</title></head>")
-    self.wfile.write(b"<body><p>The authentication flow has completed.</p>")
-    self.wfile.write(b"</body></html>")
+code = None
 
-  def log_message(self, format, *args):
-    """Do not log messages to stdout while running as command line program."""
+
+class AuthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        query = self.path.split('?', 1)[-1]
+        query = dict(parse_qsl(query))
+        if 'error' in query:
+            sys.exit('Authentication request was rejected.')
+        if 'code' in query:
+            global code
+            code = query['code']
+        self.server.query_params = query
+        self.wfile.write(b"<html><head><title>Authentication Status</title></head>")
+        self.wfile.write(b"<body><p>The authentication flow has completed.</p>")
+        self.wfile.write(b"</body></html>")
+
+    def log_message(self, format, *args):
+        """Do not log messages to stdout while running as command line program."""
+
 
 class API(object):
-  def __init__(self, project):
-    self.project = project
-    self.env = dict(os.environ)
+    def __init__(self, project):
+        self.project = project
+        self.env = dict(os.environ)
 
-  def auth(self):
-    httpd = HTTPServer(('localhost', 8080), AuthHandler)
-    flow = client.flow_from_clientsecrets('application_default_credentials.json',
-                                          scope='https://www.googleapis.com/auth/compute',
-                                          redirect_uri='http://localhost:8080')
-    auth_uri = flow.step1_get_authorize_url()
-    webbrowser.open_new(auth_uri)
-    httpd.handle_request()
-    credentials = flow.step2_exchange(code)
-    self.compute = build('compute', 'v1', credentials=credentials)
+    def auth(self):
+        default_cred = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    'application_default_credentials.json')
+        httpd = HTTPServer(('localhost', 8080), AuthHandler)
+        flow = client.flow_from_clientsecrets(default_cred,
+                                              scope='https://www.googleapis.com/auth/compute',
+                                              redirect_uri='http://localhost:8080')
+        auth_uri = flow.step1_get_authorize_url()
+        webbrowser.open_new(auth_uri)
+        httpd.handle_request()
+        credentials = flow.step2_exchange(code)
+        self.compute = build('compute', 'v1', credentials=credentials)
 
-  def get_regions(self):
-    regions = self.compute.zones().list(project=self.project).execute()['items']
-    return [region['name'] for region in regions]
+    def get_regions(self):
+        regions = self.compute.zones().list(project=self.project).execute()['items']
+        return [region['name'] for region in regions]
--- errbot/wizard/remote.py
+++ errbot/wizard/remote.py
@@ -1,25 +1,29 @@
 import os
 import paramiko
-## TODO
 
-privkey = paramiko.RSAKey.from_private_key_file (path_to_priv_key_file)
+path_to_priv_key_file = ''
+log_filename = ''
+server = ''
+username = ''
+
+privkey = paramiko.RSAKey.from_private_key_file(path_to_priv_key_file)
 ssh = paramiko.SSHClient()
-parmiko.util.log_to_file(log_filename)
+paramiko.util.log_to_file(log_filename)
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#In case the server's key is unknown,
-#we will be adding it automatically to the list of known hosts
-ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-#Loads the user's local known host file.
-ssh.connect(server, username=username,pkey=privkey )
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls /tmp')
-print "output", ssh_stdout.read() #Reading output of the executed command
-error = ssh_stderr.read()
-#Reading the error stream of the executed command
-print "err", error, len(error) 
 
-#Transfering files to and from the remote machine
-sftp = ssh.open_sftp()
-sftp.get(remote_path, local_path)
-sftp.put(local_path, remote_path)
-sftp.close()
+# In case the server's key is unknown,
+# we will be adding it automatically to the list of known hosts
+ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+# Loads the user's local known host file.
+ssh.connect(server, username=username, pkey=privkey)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls /tmp')
+print("output", ssh_stdout.read())
+error = ssh_stderr.read()
+print("err", error, len(error))
+
+# Transfering files to and from the remote machine
+# sftp = ssh.open_sftp()
+# sftp.get(remote_path, local_path)
+# sftp.put(local_path, remote_path)
+# sftp.close()
 ssh.close()

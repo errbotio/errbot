62fd4833078deab1dd064c7497b368e4c431c4b5
Guillaume Binet <gbin@google.com>
2015-10-24 21:14:38 -0700

    alpha sorted

--- tools/gen_home.py
+++ tools/gen_home.py
@@ -5,6 +5,7 @@ template = Template(open('plugins.md').read())
 
 with open('plugins.txt', 'r') as p:
     plugins = [eval(line) for line in p]
+    plugins = sorted(plugins, key=lambda plug: plug['name'])
 
     with open('Home.md', 'w') as out:
         out.write(template.render(plugins=plugins))

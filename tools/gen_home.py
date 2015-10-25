d49f79094044b561e31c98eb9f7d59cecf64d61e
Guillaume Binet <gbin@google.com>
2015-10-25 09:49:14 -0700

    Better dupe detection.

--- tools/gen_home.py
+++ tools/gen_home.py
@@ -5,6 +5,11 @@ template = Template(open('plugins.md').read())
 
 with open('plugins.txt', 'r') as p:
     plugins = [eval(line) for line in p]
+    # Removes the weird forks of errbot itself.
+    plugins = [plugin for plugin in plugins if not plugin['path'].startswith('errbot')]
+
+    # Removes dupes from the same repos.
+    plugins = {(plugin['repo'], plugin['name']): plugin for plugin in plugins}.values()
     plugins = sorted(plugins, key=lambda plug: plug['name'])
 
     with open('Home.md', 'w') as out:

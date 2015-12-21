08be9880ca65ea087399ce6bc168c8ae688d973d
Sijis Aviles <sijis.aviles+github@gmail.com>
2015-12-20 21:38:58 -0600

    Update gen_home.py and plugins.md to use repos.json

--- tools/gen_home.py
+++ tools/gen_home.py
@@ -2,30 +2,28 @@
 from jinja2 import Template
 import requests
 import time
+import ast
 
 template = Template(open('plugins.md').read())
 
 blacklisted = [repo.strip() for repo in open('blacklisted.txt', 'r').readlines()]
 
-DEFAULT_AVATAR = 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Err-logo.png'
+with open('repos.json', 'r') as p:
+    plugins = ast.literal_eval(p.read())
 
-with open('plugins.txt', 'r') as p:
-    plugins = [eval(line) for line in p]
-    # Removes the weird forks of errbot itself.
-    plugins = [plugin for plugin in plugins if not plugin['path'].startswith('errbot')]
+    # Removes the weird forks of errbot itself and
+    # blacklisted repos
+    for plugin, values in plugins.items():
+        if values['path'].startswith('errbot') or \
+           plugin in blacklisted:
+            plugins.pop(plugin)
 
-    # Be sure to remove the blacklisted ones.
-    plugins = [plugin for plugin in plugins if plugin['repo'] not in blacklisted]
-
-    # Removes dupes from the same repos.
-    plugins = {(plugin['repo'], plugin['name']): plugin for plugin in plugins}.values()
-
-    plugins = sorted(plugins, key=lambda plug: plug['name'])
+    sorted_plugins = sorted(plugins.items())
 
     with open('sorted-dedupped-plugins.txt', 'w') as out:
-        for plugin in plugins:
+        for plugin in sorted_plugins:
             out.write(repr(plugin))
             out.write('\n')
 
     with open('Home.md', 'w') as out:
-        out.write(template.render(plugins=plugins))
+        out.write(template.render(plugins=sorted_plugins))

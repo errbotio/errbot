575a247e346341fdc8e9a88486b1487769f1222c
Guillaume Binet <gbin@google.com>
2015-12-28 15:58:13 -0800

    Added multiple mentions test.

--- tests/mention_plugin/mention.py
+++ tests/mention_plugin/mention.py
@@ -6,4 +6,5 @@ class MentionTestPlugin(BotPlugin):
         if self.bot_identifier in people:
             self.send(message.frm, "Somebody mentioned me!", message_type=message.type)
         else:
-            self.send(message.frm, "Somebody mentioned %s!" % ','.join(p.person for p in people), message_type=message.type)
+            self.send(message.frm, "Somebody mentioned %s!" %
+                      ','.join(p.person for p in people), message_type=message.type)

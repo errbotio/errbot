3832944ace0bd15fda630c8358ad737737298806
Guillaume Binet <gbin@gootz.net>
2016-01-20 20:27:53 -0800

    backward compatibility on self.shelf
    - this is triggering a deprecated warning (it was not meant to be used
      in the first place).
    - move self.store to self._store so it won't happen again on self.store.

--- errbot/storage/__init__.py
+++ errbot/storage/__init__.py
@@ -26,47 +26,52 @@ class StoreMixin(MutableMapping):
 
     def __init__(self):
         log.info('Init storage of %s' % self.__class__.__name__)
-        self.store = None
+        self._store = None
+
+    @property
+    def shelf(self):
+        log.warn('Deprecated: you should use self instead of self.shelf to access your storage.')
+        return self
 
     def open_storage(self, storage_plugin, namespace):
-        if hasattr(self, 'store') and self.store is not None:
+        if hasattr(self, 'store') and self._store is not None:
             raise StoreAlreadyOpenError("Storage appears to be opened already")
         log.debug("Opening storage %s" % namespace)
-        self.store = storage_plugin.open(namespace)
+        self._store = storage_plugin.open(namespace)
 
     def close_storage(self):
-        if not hasattr(self, 'store') or self.store is None:
+        if not hasattr(self, 'store') or self._store is None:
             raise StoreNotOpenError("Storage does not appear to have been opened yet")
-        self.store.close()
-        self.store = None
+        self._store.close()
+        self._store = None
         log.debug('Closed store of %s' % self.__class__.__name__)
 
     # those are the minimal things to behave like a dictionary with the UserDict.DictMixin
     def __getitem__(self, key):
-        return self.store.get(key)
+        return self._store.get(key)
 
     def __setitem__(self, key, item):
-        return self.store.set(key, item)
+        return self._store.set(key, item)
 
     def __delitem__(self, key):
-        return self.store.remove(key)
+        return self._store.remove(key)
 
     def keys(self):
-        keys = self.store.keys()
+        keys = self._store.keys()
         if PY2:
             keys = [key.decode('utf-8') for key in keys]
         return keys
 
     def __len__(self):
-        return self.store.len()
+        return self._store.len()
 
     def __iter__(self):
-        for i in self.store.keys():
+        for i in self._store.keys():
             yield i
 
     def __contains__(self, x):
         try:
-            self.store.get(x)
+            self._store.get(x)
             return True
         except KeyError:
             return False

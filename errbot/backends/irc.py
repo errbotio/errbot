dc0e8e448db55bc30e81d7431a402596e759cb66
Guillaume Binet <gbin@gootz.net>
2016-02-11 12:41:43 -0800

    don't callback on failure.

--- errbot/backends/irc.py
+++ errbot/backends/irc.py
@@ -171,6 +171,7 @@ class IRCMUCRoom(MUCRoom):
             time.sleep(.5)
         else:
             self.log.error("Timeout: Could not join %s", self.room)
+            return
 
         self._bot.callback_room_joined(self)
         log.info("Joined room {}".format(self.room))
@@ -193,6 +194,8 @@ class IRCMUCRoom(MUCRoom):
             time.sleep(.5)
         else:
             self.log.error("Timeout: Could not part %s", self.room)
+            return
+
         log.info("Left room {}".format(self.room))
         self._bot.callback_room_left(self)
 

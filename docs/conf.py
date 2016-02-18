d6a58c59b2fd1bb5fde250000d973a5cc48a0da1
Nick Groenen <nick@byte.nl>
2016-02-18 16:44:02 +0100

    Add sphinx-autodoc-annotation

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,11 @@ from errbot.version import VERSION
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_annotation',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

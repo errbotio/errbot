774451a7eba984c301a53a40f7fc4d208d0938e7
Guillaume Binet <gbin@google.com>
2016-02-19 10:34:09 -0800

    triple quotes

--- errbot/repos.py
+++ errbot/repos.py
@@ -5,7 +5,7 @@ import ast
 
 
 def get_known_repos():
-    '''
+    """
     Get known repos from registry
 
     An example entry of the json file is the following
@@ -15,7 +15,7 @@ def get_known_repos():
         'path': 'https://github.com/errbotio/err-pypi.git',
         'python': None
     }, ...
-    '''
+    """
     registry_url = 'http://bit.ly/1kjdlRX'
     registry = urllib.request.urlopen(registry_url).read()
     return ast.literal_eval(registry.decode('utf-8'))

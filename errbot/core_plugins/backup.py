08505f7356dfd1e86f3e162bb9e909ba97002d7d
Guillaume Binet <gbin@google.com>
2016-03-02 15:14:51 -0800

    Made a backupv4 for v3

--- errbot/core_plugins/backup.py
+++ errbot/core_plugins/backup.py
@@ -38,3 +38,35 @@ class Backup(BotPlugin):
                     f.write('pobj.close_storage()\n')
 
         return "The backup file has been written in '%s'" % filename
+
+    @botcmd(admin_only=True)
+    def backupv4(self, msg, args):
+        """Backup everything.
+           Makes a backup script called backup.py in the data bot directory.
+           You can restore the backup from the command line with err.py --restore
+        """
+        filename = os.path.join(self.bot_config.BOT_DATA_DIR, 'backupv4.py')
+        with open(filename, 'w') as f:
+            f.write('## This file is not executable on its own. use errbot -r FILE to restore your bot.\n\n')
+            f.write('log.info("Restoring repo_manager.")\n')
+            for key, value in self._bot.items():
+                f.write('bot.repo_manager["'+key+'"] = ' + repr(value) + '\n')
+
+            f.write('log.info("Installing plugins.")\n')
+            f.write('if "installed_repos" in bot.repo_manager:\n')
+            f.write('  for repo in bot.repo_manager["installed_repos"]:\n')
+            f.write('    log.error(bot.repo_manager.install_repo(repo))\n')
+
+            f.write('log.info("Restoring plugins data.")\n')
+            f.write('bot.plugin_manager.update_dynamic_plugins()\n')
+            for plug in self._bot.getAllPlugins():
+                pobj = plug.plugin_object
+                if pobj.shelf:
+                    f.write('pobj = bot.plugin_manager.get_plugin_by_name("' + plug.name + '").plugin_object\n')
+                    f.write('pobj.init_storage()\n')
+
+                    for key, value in pobj.items():
+                        f.write('pobj["'+key+'"] = ' + repr(value) + '\n')
+                    f.write('pobj.close_storage()\n')
+
+        return "The backup file has been written in '%s'" % filename

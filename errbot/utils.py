e3a5bd4c7ca4b49427398a554ca9cf4956629609
Guillaume Binet <gbin@google.com>
2016-03-10 15:25:01 -0800

    PEP hating

--- errbot/utils.py
+++ errbot/utils.py
@@ -315,6 +315,7 @@ def collect_roots(base_paths, file_sig='*.plug'):
             result |= find_roots(path_or_list, file_sig)
     return result
 
+
 def ensure_sys_path_contains(paths):
     """ Ensure that os.path contains paths
        :param base_paths:
@@ -326,6 +327,7 @@ def ensure_sys_path_contains(paths):
             ensure_sys_path_contains(entry)
         elif entry is not None and entry not in sys.path:
             sys.path.append(entry)
+
 
 def get_class_that_defined_method(meth):
     for cls in inspect.getmro(type(meth.__self__)):

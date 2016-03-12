33c004268cdbfd80967d2cafb1aafaf6ac9184f3
Nick Groenen <nick@groenen.me>
2016-03-12 16:37:10 +0100

    Pass args to run_tests.py through to py.test
    This makes it easy to run tests in parallell (-n 4) or have pytest drop
    you into a debugger on failure (--pdb).
    
    This way it's not necessary to invoke py.test directly, which would skip
    other tests that run_tests.py performs.

--- run_tests.py
+++ run_tests.py
@@ -3,13 +3,17 @@
 import pytest
 import subprocess
 import sys
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
 
-PY3 = sys.version_info >= (3, 0, 0)
+PY2 = sys.version_info < (3, 0, 0)
 
 pep8_result = 0
 pypi_linting = 0
 
-if PY3:
+if not PY2:
     # py.test has a pep8 plugin, however it has considerably fewer options
     # for configuration, hence the use of the stand-alone pep8 checker instead.
     pep8_result = subprocess.call(['pep8', '--statistics', '--show-source'])
@@ -30,7 +34,7 @@ if pypi_linting != 0:
     print("PyPI linting failed (check README.rst and CHANGES.rst).")
     sys.exit(-2)
 
-pytest_result = pytest.main('-x')
+pytest_result = pytest.main(" ".join([quote(arg) for arg in sys.argv[1:]]))
 
 if pytest_result or pep8_result:
     sys.exit(1)

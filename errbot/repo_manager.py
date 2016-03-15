f383c1bda96d04b03dc2cf7ef9ae1dbcd1db3fed
Guillaume Binet <gbin@google.com>
2016-03-15 13:10:04 -0700

    Fix: Uninstall corrupting the repos entries.
    Fixes #674

--- errbot/repo_manager.py
+++ errbot/repo_manager.py
@@ -244,6 +244,7 @@ class BotRepoManager(StoreMixin):
 
     def uninstall_repo(self, name):
         repo_path = path.join(self.plugin_dir, name)
-        shutil.rmtree(repo_path)
-        repos = self.get_installed_plugin_repos().pop(name)
+        shutil.rmtree(repo_path, ignore_errors=True)  # ignore errors because the DB can be desync'ed from the file tree.
+        repos = self.get_installed_plugin_repos()
+        del(repos[name])
         self.set_plugin_repos(repos)

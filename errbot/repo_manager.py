5e7bd14051b3e95b1cf8d2d9dfc4130f7854f165
Guillaume Binet <gbin@google.com>
2016-03-15 14:40:12 -0700

    pep8

--- errbot/repo_manager.py
+++ errbot/repo_manager.py
@@ -244,7 +244,8 @@ class BotRepoManager(StoreMixin):
 
     def uninstall_repo(self, name):
         repo_path = path.join(self.plugin_dir, name)
-        shutil.rmtree(repo_path, ignore_errors=True)  # ignore errors because the DB can be desync'ed from the file tree.
+        # ignore errors because the DB can be desync'ed from the file tree.
+        shutil.rmtree(repo_path, ignore_errors=True)
         repos = self.get_installed_plugin_repos()
         del(repos[name])
         self.set_plugin_repos(repos)

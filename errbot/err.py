55b833f6e940c4480665f976c538c6b5281c7811
Guillaume Binet <gbin@google.com>
2016-04-07 15:56:55 -0700

    weird copy/paste.

--- errbot/err.py
+++ errbot/err.py
@@ -57,7 +57,7 @@ log = logging.getLogger(__name__)
 def debug(sig, frame):
     """Interrupt running process, and provide a python prompt for
     interactive debugging."""
-    d = {'_frame': frame}  # import sys Allow access to frame object.
+    d = {'_frame': frame}  # Allow access to frame object.
     d.update(frame.f_globals)  # Unless shadowed by global
     d.update(frame.f_locals)
 

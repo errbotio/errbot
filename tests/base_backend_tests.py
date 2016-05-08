e6e3bad0dfdefea5c64b73c33554c557aef5637f
Guillaume Binet <gbin@gootz.net>
2016-05-08 16:30:45 -0700

    ported utils tests

--- tests/base_backend_tests.py
+++ tests/base_backend_tests.py
@@ -14,7 +14,7 @@ from collections import OrderedDict
 from queue import Queue, Empty  # noqa
 from errbot.errBot import ErrBot
 from errbot.backends.base import Message, Room, Identifier, ONLINE
-from errbot.backends.test import TestPerson, TestOccupant, TestRoom
+from errbot.backends.test import TestPerson, TestOccupant, TestRoom, ShallowConfig
 from errbot import botcmd, re_botcmd, arg_botcmd, templating  # noqa
 from errbot.main import CORE_STORAGE
 from errbot.plugin_manager import BotPluginManager
@@ -42,10 +42,6 @@ SIMPLE_JSON_PLUGINS_INDEX = """
     }
 }
 """
-
-
-class ShallowConfig(object):
-    pass
 
 
 class DummyBackend(ErrBot):
--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 from datetime import timedelta
-import unittest
-from nose.tools import raises
+import pytest
 
+from errbot.backends.test import ShallowConfig
 from errbot.errBot import bot_config_defaults
 from errbot.main import CORE_STORAGE
 from errbot.specific_plugin_manager import SpecificPluginManager
@@ -14,7 +14,12 @@ log = logging.getLogger(__name__)
 
 
 def vc(v1, v2):
-    return version2array(v1) < version2array(v2)
+    assert version2array(v1) < version2array(v2)
+
+
+def vc_neg(version):
+    with pytest.raises(ValueError):
+        version2array(version)
 
 
 def test_version_check():
@@ -30,106 +35,122 @@ def test_version_check():
 
 
 def test_version_check_negative():
-    raises(ValueError)(version2array)('1.2.3.4', )
-    raises(ValueError)(version2array)('1.2', )
-    raises(ValueError)(version2array)('1.2.-beta', )
-    raises(ValueError)(version2array)('1.2.3-toto', )
-    raises(ValueError)(version2array)('1.2.3-rc', )
+    yield vc_neg, '1.2.3.4'
+    yield vc_neg, '1.2'
+    yield vc_neg, '1.2.-beta'
+    yield vc_neg, '1.2.3-toto'
+    yield vc_neg, '1.2.3-rc'
 
 
-class TestUtils(unittest.TestCase):
-    def test_formattimedelta(self):
-        td = timedelta(0, 60 * 60 + 13 * 60)
-        self.assertEqual('1 hours and 13 minutes', format_timedelta(td))
+def test_formattimedelta():
+    td = timedelta(0, 60 * 60 + 13 * 60)
+    assert '1 hours and 13 minutes' == format_timedelta(td)
 
-    def test_drawbar(self):
-        self.assertEqual(drawbar(5, 10), '[████████▒▒▒▒▒▒▒]')
-        self.assertEqual(drawbar(0, 10), '[▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒]')
-        self.assertEqual(drawbar(10, 10), '[███████████████]')
 
-    def unescape_test(self):
-        self.assertEqual(unescape_xml('&#32;'), ' ')
+def test_drawbar():
+    assert drawbar(5, 10) == '[████████▒▒▒▒▒▒▒]'
+    assert drawbar(0, 10) == '[▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒]'
+    assert drawbar(10, 10) == '[███████████████]'
 
-    def test_storage(self):
-        key = b'test' if PY2 else 'test'
-        config = sys.modules['errbot.config-template']
-        bot_config_defaults(config)
 
-        spm = SpecificPluginManager(config, 'storage', StoragePluginBase, CORE_STORAGE, None)
-        storage_plugin = spm.get_plugin_by_name('Memory')
+def unescape_test():
+    assert unescape_xml('&#32;') == ' '
 
-        persistent_object = StoreMixin()
-        persistent_object.open_storage(storage_plugin, 'test')
-        persistent_object[key] = 'à value'
-        self.assertEquals(persistent_object[key], 'à value')
-        self.assertIn(key, persistent_object)
-        del persistent_object[key]
-        self.assertNotIn(key, persistent_object)
-        self.assertEquals(len(persistent_object), 0)
 
-    def test_recurse_check_structure_valid(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={'foo': "Bar"}, none=None, true=True,
-                        false=False)
+def test_storage():
+    key = b'test' if PY2 else 'test'
+
+    __import__('errbot.config-template')
+    config = ShallowConfig()
+    config.__dict__.update(sys.modules['errbot.config-template'].__dict__)
+    bot_config_defaults(config)
+
+    spm = SpecificPluginManager(config, 'storage', StoragePluginBase, CORE_STORAGE, None)
+    storage_plugin = spm.get_plugin_by_name('Memory')
+
+    persistent_object = StoreMixin()
+    persistent_object.open_storage(storage_plugin, 'test')
+    persistent_object[key] = 'à value'
+    assert persistent_object[key] == 'à value'
+    assert key in persistent_object
+    del persistent_object[key]
+    assert key not in persistent_object
+    assert len(persistent_object) == 0
+
+
+def test_recurse_check_structure_valid():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={'foo': "Bar"}, none=None, true=True,
+                    false=False)
+    recurse_check_structure(sample, to_check)
+
+
+def test_recurse_check_structure_missingitem():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_missingitem(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True)
+
+def test_recurse_check_structure_extrasubitem():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={'foo': "Bar", 'Bar': "Foo"}, none=None,
+                    true=True, false=False)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_extrasubitem(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={'foo': "Bar", 'Bar': "Foo"}, none=None,
-                        true=True, false=False)
+
+def test_recurse_check_structure_missingsubitem():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={}, none=None, true=True, false=False)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_missingsubitem(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list=["Foo", "Bar", "Bas"], dict={}, none=None, true=True, false=False)
+
+def test_recurse_check_structure_wrongtype_1():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string=None, list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_wrongtype_1(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string=None, list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+
+def test_recurse_check_structure_wrongtype_2():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list={'foo': "Bar"}, dict={'foo': "Bar"}, none=None, true=True, false=False)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_wrongtype_2(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list={'foo': "Bar"}, dict={'foo': "Bar"}, none=None, true=True, false=False)
+
+def test_recurse_check_structure_wrongtype_3():
+    sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
+    to_check = dict(string="Foobar", list=["Foo", "Bar"], dict=["Foo", "Bar"], none=None, true=True, false=False)
+    with pytest.raises(ValidationException):
         recurse_check_structure(sample, to_check)
 
-    @raises(ValidationException)
-    def test_recurse_check_structure_wrongtype_3(self):
-        sample = dict(string="Foobar", list=["Foo", "Bar"], dict={'foo': "Bar"}, none=None, true=True, false=False)
-        to_check = dict(string="Foobar", list=["Foo", "Bar"], dict=["Foo", "Bar"], none=None, true=True, false=False)
-        recurse_check_structure(sample, to_check)
 
-    def test_split_string_after_returns_original_string_when_chunksize_equals_string_size(self):
-        str_ = 'foobar2000' * 2
-        splitter = split_string_after(str_, len(str_))
-        split = [chunk for chunk in splitter]
-        self.assertEqual([str_], split)
+def test_split_string_after_returns_original_string_when_chunksize_equals_string_size():
+    str_ = 'foobar2000' * 2
+    splitter = split_string_after(str_, len(str_))
+    split = [chunk for chunk in splitter]
+    assert [str_] == split
 
-    def test_split_string_after_returns_original_string_when_chunksize_equals_string_size_plus_one(self):
-        str_ = 'foobar2000' * 2
-        splitter = split_string_after(str_, len(str_) + 1)
-        split = [chunk for chunk in splitter]
-        self.assertEqual([str_], split)
 
-    def test_split_string_after_returns_two_chunks_when_chunksize_equals_string_size_minus_one(self):
-        str_ = 'foobar2000' * 2
-        splitter = split_string_after(str_, len(str_) - 1)
-        split = [chunk for chunk in splitter]
-        self.assertEqual(['foobar2000foobar200', '0'], split)
+def test_split_string_after_returns_original_string_when_chunksize_equals_string_size_plus_one():
+    str_ = 'foobar2000' * 2
+    splitter = split_string_after(str_, len(str_) + 1)
+    split = [chunk for chunk in splitter]
+    assert [str_] == split
 
-    def test_split_string_after_returns_two_chunks_when_chunksize_equals_half_length_of_string(self):
-        str_ = 'foobar2000' * 2
-        splitter = split_string_after(str_, int(len(str_) / 2))
-        split = [chunk for chunk in splitter]
-        self.assertEqual(['foobar2000', 'foobar2000'], split)
+
+def test_split_string_after_returns_two_chunks_when_chunksize_equals_string_size_minus_one():
+    str_ = 'foobar2000' * 2
+    splitter = split_string_after(str_, len(str_) - 1)
+    split = [chunk for chunk in splitter]
+    assert ['foobar2000foobar200', '0'] == split
+
+
+def test_split_string_after_returns_two_chunks_when_chunksize_equals_half_length_of_string():
+    str_ = 'foobar2000' * 2
+    splitter = split_string_after(str_, int(len(str_) / 2))
+    split = [chunk for chunk in splitter]
+    assert ['foobar2000', 'foobar2000'] == split

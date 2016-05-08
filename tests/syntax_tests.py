32e3dc72350d8e7e11b3bb1468a2718bb8273fd2
Guillaume Binet <gbin@gootz.net>
2016-05-08 16:09:36 -0700

    ported syntax tests.

--- tests/syntax_tests.py
+++ tests/syntax_tests.py
@@ -1,24 +1,20 @@
-# coding=utf-8
-from errbot.backends.test import FullStackTest
+from errbot.backends.test import testbot
 from os import path
 
+extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'syntax_plugin')
 
-class TestSyntax(FullStackTest):
 
-    def setUp(self, *args, **kwargs):
-        kwargs['extra_plugin_dir'] = path.join(path.dirname(
-            path.realpath(__file__)), 'syntax_plugin')
+def test_nosyntax(testbot):
+    assert testbot.bot.commands['foo_nosyntax']._err_command_syntax is None
 
-        super().setUp(*args, **kwargs)
 
-    def test_nosyntax(self):
-        self.assertIsNone(self.bot.commands['foo_nosyntax']._err_command_syntax)
+def test_syntax(testbot):
+    assert testbot.bot.commands['foo']._err_command_syntax == '[optional] <mandatory>'
 
-    def test_syntax(self):
-        self.assertEquals(self.bot.commands['foo']._err_command_syntax, '[optional] <mandatory>')
 
-    def test_re_syntax(self):
-        self.assertEquals(self.bot.re_commands['re_foo']._err_command_syntax, '.*')
+def test_re_syntax(testbot):
+    assert testbot.bot.re_commands['re_foo']._err_command_syntax == '.*'
 
-    def test_arg_syntax(self):
-        self.assertEquals(self.bot.commands['arg_foo']._err_command_syntax, '[-h] [--repeat-count REPEAT] value')
+
+def test_arg_syntax(testbot):
+    assert testbot.bot.commands['arg_foo']._err_command_syntax == '[-h] [--repeat-count REPEAT] value'

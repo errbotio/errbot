5d56f816b6540a4c768d7130df425c563edfbf8d
Guillaume Binet <gbin@gootz.net>
2016-05-08 15:11:55 -0700

    ported i18n_tests

--- tests/i18n_tests.py
+++ tests/i18n_tests.py
@@ -8,18 +8,18 @@ extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'i18n_plugin
 
 
 def test_i18n_return(testbot):
-    testbot.assertCommand('!i18n 1', 'язы́к')
+    assert 'язы́к' in testbot.exec_command('!i18n 1')
 
 
 def test_i18n_simple_name(testbot):
-    testbot.assertCommand('!ру́сский', 'OK')
+    assert 'OK' in testbot.exec_command('!ру́сский')
 
 
 def test_i18n_prefix(testbot):
-    testbot.assertCommand('!prefix_ру́сский', 'OK')
-    testbot.assertCommand('!prefix ру́сский', 'OK')
+    assert 'OK' in testbot.exec_command('!prefix_ру́сский')
+    assert 'OK' in testbot.exec_command('!prefix ру́сский')
 
 
 def test_i18n_suffix(testbot):
-    testbot.assertCommand('!ру́сский_suffix', 'OK')
-    testbot.assertCommand('!ру́сский suffix', 'OK')
+    assert 'OK' in testbot.exec_command('!ру́сский_suffix')
+    assert 'OK' in testbot.exec_command('!ру́сский suffix')

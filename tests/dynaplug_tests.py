f8ba63365a63cc8d748c7f4dd71b654b676f75b3
Guillaume Binet <gbin@gootz.net>
2016-05-08 14:41:30 -0700

    ported dynaplig_tests

--- tests/dynaplug_tests.py
+++ tests/dynaplug_tests.py
@@ -1,43 +1,38 @@
-# coding=utf-8
-from errbot.backends.test import FullStackTest
-from flaky import flaky
 from os import path
 
+from errbot.backends.test import testbot  # noqa
 
-@flaky
-class TestDynaPlugins(FullStackTest):
+extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'dyna_plugin')
 
-    def setUp(self, *args, **kwargs):
-        kwargs['extra_plugin_dir'] = path.join(path.dirname(
-            path.realpath(__file__)), 'dyna_plugin')
 
-        super().setUp(*args, **kwargs)
+def test_simple(testbot):
+    assert 'added' in testbot.exec_command('!add_simple')
+    assert 'yep' in testbot.exec_command('!say_yep')
+    assert 'foo' in testbot.exec_command('!say_foo')
+    assert 'documented' in testbot.exec_command('!help')
+    assert 'removed' in testbot.exec_command('!remove_simple')
+    assert 'Command "say_foo" not found' in testbot.exec_command('!say_foo')
 
-    def test_simple(self):
-        self.assertCommand('!add_simple', 'added')
-        self.assertCommand('!say_yep', 'yep')
-        self.assertCommand('!say_foo', 'foo')
-        self.assertCommand('!help', 'documented')
-        self.assertCommand('!remove_simple', 'removed')
-        self.assertCommand('!say_foo', 'Command "say_foo" not found')
 
-    def test_re(self):
-        self.assertCommand('!add_re', 'added')
-        self.assertCommand('I said cheese', 'fffound')
-        self.assertCommand('!remove_re', 'removed')
+def test_re(testbot):
+    assert 'added' in testbot.exec_command('!add_re')
+    assert 'fffound' in testbot.exec_command('I said cheese')
+    assert 'removed' in testbot.exec_command('!remove_re')
 
-    def test_saw(self):
-        self.assertCommand('!add_saw', 'added')
-        self.assertCommand('!splitme foo,bar,baz', 'foo+bar+baz')
-        self.assertCommand('!remove_saw', 'removed')
 
-    def test_clashing(self):
-        self.assertCommand('!clash', 'original')
-        self.assertCommand('!add_clashing',
-                           'clashing.clash clashes with Dyna.clash so it has been renamed clashing-clash')
-        self.assertIn('added', self.bot.pop_message())
-        self.assertCommand('!clash', 'original')
-        self.assertCommand('!clashing-clash', 'dynamic')
-        self.assertCommand('!remove_clashing', 'removed')
-        self.assertCommand('!clash', 'original')
-        self.assertCommand('!clashing-clash', 'not found')
+def test_saw(testbot):
+    assert 'added' in testbot.exec_command('!add_saw')
+    assert 'foo+bar+baz' in testbot.exec_command('!splitme foo,bar,baz')
+    assert 'removed' in testbot.exec_command('!remove_saw')
+
+
+def test_clashing(testbot):
+    assert 'original' in testbot.exec_command('!clash')
+    assert 'clashing.clash clashes with Dyna.clash so it has been renamed clashing-clash' in \
+           testbot.exec_command('!add_clashing')
+    assert 'added' in testbot.pop_message()
+    assert 'original' in testbot.exec_command('!clash')
+    assert 'dynamic' in testbot.exec_command('!clashing-clash')
+    assert 'removed' in testbot.exec_command('!remove_clashing')
+    assert 'original' in testbot.exec_command('!clash')
+    assert 'not found' in testbot.exec_command('!clashing-clash')

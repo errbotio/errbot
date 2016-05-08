f5eefd875d559e96fd0738f2be5114e118d771c5
Guillaume Binet <gbin@gootz.net>
2016-05-08 15:12:04 -0700

    ported md_rendering_tests

--- tests/md_rendering_test.py
+++ tests/md_rendering_test.py
@@ -1,28 +1,29 @@
 # vim: ts=4:sw=4
 import logging
-import unittest
 from errbot.rendering import ansi, text, md, md_escape
 
 log = logging.getLogger(__name__)
 
 
-class MdRendering(unittest.TestCase):
-    def test_ansi(self):
-        mdc = ansi()
-        self.assertEquals(mdc.convert("*woot*"), "\x1b[4mwoot\x1b[24m\x1b[0m")
+def test_ansi():
+    mdc = ansi()
+    assert mdc.convert("*woot*") == "\x1b[4mwoot\x1b[24m\x1b[0m"
 
-    def test_text(self):
-        mdc = text()
-        self.assertEquals(mdc.convert("*woot*"), "woot")
-        self.assertEquals(mdc.convert("# woot"), "WOOT")
 
-    def test_mde2md(self):
-        mdc = md()
-        self.assertEquals(mdc.convert("woot"), "woot")
-        self.assertEquals(mdc.convert("woot{:stuff} really{:otherstuff}"), "woot really")
+def test_text():
+    mdc = text()
+    assert mdc.convert("*woot*") == "woot"
+    assert mdc.convert("# woot") == "WOOT"
 
-    def test_escaping(self):
-        mdc = text()
-        original = '#not a title\n*not italic*\n`not code`\ntoto{not annotation}'
-        escaped = md_escape(original)
-        self.assertEquals(original, mdc.convert(escaped))
+
+def test_mde2md():
+    mdc = md()
+    assert mdc.convert("woot") == "woot"
+    assert mdc.convert("woot{:stuff} really{:otherstuff}") == "woot really"
+
+
+def test_escaping():
+    mdc = text()
+    original = '#not a title\n*not italic*\n`not code`\ntoto{not annotation}'
+    escaped = md_escape(original)
+    assert original == mdc.convert(escaped)

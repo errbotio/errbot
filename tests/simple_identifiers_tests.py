24dc887ef528710232ee21cb4e99edade07461d2
Guillaume Binet <gbin@gootz.net>
2016-05-08 16:06:01 -0700

    port identifiers test

--- tests/simple_identifiers_tests.py
+++ tests/simple_identifiers_tests.py
@@ -1,35 +1,34 @@
-import unittest
-
 from errbot.backends.test import TestPerson, TestOccupant
 
 
-class TestSimpleIdentifiers(unittest.TestCase):
-    def test_identifier_eq(self):
-        a = TestPerson("foo")
-        b = TestPerson("foo")
-        self.assertTrue(a == b)
-        self.assertEqual(a, b)
+def test_identifier_eq():
+    a = TestPerson("foo")
+    b = TestPerson("foo")
+    assert a == b
 
-    def test_identifier_ineq(self):
-        a = TestPerson("foo")
-        b = TestPerson("bar")
-        self.assertFalse(a == b)
-        self.assertNotEqual(a, b)
 
-    def test_mucidentifier_eq(self):
-        a = TestOccupant("foo", "room")
-        b = TestOccupant("foo", "room")
-        self.assertTrue(a == b)
-        self.assertEqual(a, b)
+def test_identifier_ineq():
+    a = TestPerson("foo")
+    b = TestPerson("bar")
+    assert not a == b
+    assert a != b
 
-    def test_mucidentifier_ineq1(self):
-        a = TestOccupant("foo", "room")
-        b = TestOccupant("bar", "room")
-        self.assertFalse(a == b)
-        self.assertNotEqual(a, b)
 
-    def test_mucidentifier_ineq2(self):
-        a = TestOccupant("foo", "room1")
-        b = TestOccupant("foo", "room2")
-        self.assertFalse(a == b)
-        self.assertNotEqual(a, b)
+def test_mucidentifier_eq():
+    a = TestOccupant("foo", "room")
+    b = TestOccupant("foo", "room")
+    assert a == b
+
+
+def test_mucidentifier_ineq1():
+    a = TestOccupant("foo", "room")
+    b = TestOccupant("bar", "room")
+    assert not a == b
+    assert a != b
+
+
+def test_mucidentifier_ineq2():
+    a = TestOccupant("foo", "room1")
+    b = TestOccupant("foo", "room2")
+    assert not a == b
+    assert a != b

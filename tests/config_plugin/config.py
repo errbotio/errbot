5d571b8ae824ac931499df833d2585491b4c121a
Guillaume Binet <gbin@gootz.net>
2016-05-08 18:00:38 -0700

    Repro test for #737.

---
+++ tests/config_plugin/config.py
@@ -0,0 +1,9 @@
+from errbot import BotPlugin
+
+
+class Config(BotPlugin):
+    """
+    Just a plugin with a simple string config.
+    """
+    def get_configuration_template(self):
+        return {'One': 'one'}
---
+++ tests/plugin_config.py
@@ -0,0 +1,9 @@
+from os import path
+
+from errbot.backends.test import testbot
+
+extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'config_plugin')
+
+
+def test_failed_config(testbot):
+    assert 'Plugin configuration done.' in testbot.exec_command('!plugin config Config {"One": "two"}')

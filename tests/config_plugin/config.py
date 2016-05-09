65f1e318bfae78efc4f1bcdffb3b2e36954897ef
Guillaume Binet <gbin@gootz.net>
2016-05-08 18:07:50 -0700

    avoid unicode conversion on py2.

--- tests/config_plugin/config.py
+++ tests/config_plugin/config.py
@@ -1,4 +1,5 @@
 from errbot import BotPlugin
+from errbot.err import PY3
 
 
 class Config(BotPlugin):
@@ -6,4 +7,6 @@ class Config(BotPlugin):
     Just a plugin with a simple string config.
     """
     def get_configuration_template(self):
-        return {'One': 'one'}
+        if PY3:
+            return {'One': 'one'}
+        return {'One'.encode('utf-8'): 'one'.encode('utf-8')}  # forces bytes on py2 for the test

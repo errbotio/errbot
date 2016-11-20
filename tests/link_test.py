b3aa68eea34e74820cf687ba16768c0dec571372
Guillaume Binet <gbin@gootz.net>
2016-11-20 11:12:10 -0500

    codestyle

--- tests/link_test.py
+++ tests/link_test.py
@@ -1,11 +1,11 @@
 # coding=utf-8
-from os import path, mkdir
+from os import path
 
 import pytest
 
 extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'test_link')
 
+
 def test_linked_plugin_here(testbot):
     testbot.push_message('!status plugins')
     assert 'Dummy' in testbot.pop_message()
-

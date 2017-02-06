298243934cd7a2fd10a8c390dd73dae0504040da
Nick Groenen <nick@groenen.me>
2017-02-06 23:05:35 +0100

    Slack: compatibility with slackclient 1.0.5 (#961)
    Slackclient introduced changes to their underlying client with this
    release which broke user ID lookups for errbot (see #949). With these
    changes user ID lookups will work with this new version.
    
    Quick testing shows this also continues to work with 1.0.2 and 1.0.4 but
    not 1.0.3. Earlier versions were untested.

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
             'graphic':  ['PySide', ],
             'hipchat': ['hypchat', 'sleekxmpp', 'pyasn1', 'pyasn1-modules'],
             'IRC': ['irc', ],
-            'slack': ['slackclient>=1.0.0', ],
+            'slack': ['slackclient>=1.0.5', ],
             'telegram': ['python-telegram-bot', ],
             'XMPP': ['sleekxmpp', 'pyasn1', 'pyasn1-modules'],
         },

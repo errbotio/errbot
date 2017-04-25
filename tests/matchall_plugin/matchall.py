25c6328976bdcb1fd59dff0207b6fa0bdd69692b
Guillaume Binet <gbin@gootz.net>
2017-04-25 18:30:28 -0400

    make the linter happy

--- tests/matchall_plugin/matchall.py
+++ tests/matchall_plugin/matchall.py
@@ -1,6 +1,6 @@
 from errbot import BotPlugin, botmatch
 
- 
+
 class MatchAll(BotPlugin):
     @botmatch(r".*")
     def all(self, msg, match):
--- tests/matchall_test.py
+++ tests/matchall_test.py
@@ -5,8 +5,10 @@ import pytest
 
 extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'matchall_plugin')
 
+
 def test_botmatch_correct(testbot):
     assert 'Works!' in testbot.exec_command('hi hi hi')
+
 
 def test_botmatch(testbot):
     assert 'Works!' in testbot.exec_command('123123')

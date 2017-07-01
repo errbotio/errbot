46849495fe9b8cb73b3da091163362a8d94032d2
Guillaume Binet <gbin@gootz.net>
2017-07-01 12:25:26 -0400

    Fixes #1043

--- errbot/__init__.py
+++ errbot/__init__.py
@@ -400,6 +400,8 @@ def _tag_webhook(func, uri_rule, methods, form_param, raw):
     func._err_webhook_raw = raw
     return func
 
+def _uri_from_func(func):
+    return r'/' + func.__name__
 
 def webhook(*args,
             methods: Tuple[str]=('POST', 'GET'),
@@ -438,6 +440,13 @@ def webhook(*args,
             pass
     """
 
+    if not args:  # default uri_rule but with kwargs.
+        return lambda func: _tag_webhook(func,
+                                         _uri_from_func(func),
+                                         methods=methods,
+                                         form_param=form_param,
+                                         raw=raw)
+
     if isinstance(args[0], str):  # first param is uri_rule.
         return lambda func: _tag_webhook(func,
                                          args[0] if args[0] == '/'
@@ -445,8 +454,8 @@ def webhook(*args,
                                          methods=methods,
                                          form_param=form_param,
                                          raw=raw)
-    return _tag_webhook(args[0],
-                        r'/' + args[0].__name__,
+    return _tag_webhook(args[0],  # naked decorator so the first parameter is a function.
+                        _uri_from_func(args[0]),
                         methods=methods,
                         form_param=form_param,
                         raw=raw)

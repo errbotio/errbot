1a77fff71cc9a93279131a16172e820218b05c34
Guillaume Binet <gbin@gootz.net>
2017-07-04 17:06:22 -0400

    inroom/inperson asuser/asadmin on Text Backend (#1054)
    * inroom/inperson asuser/asadmin on Text Backend
    
    This allows to simulate the person from which you send a command and
    from where (room or in person).
    
    * linting
    
    * It now passes the backend tester.
    
    * Added introduction to the text mode.
    
    * nits from Zoni.

--- errbot/backends/text.py
+++ errbot/backends/text.py
@@ -5,14 +5,16 @@ import sys
 from time import sleep
 import re
 
+import copyreg
 from ansi.color import fg, fx
 from pygments import highlight
 from pygments.formatters import Terminal256Formatter
 from pygments.lexers import get_lexer_by_name
 
+from errbot import botcmd, BotPlugin
 from errbot.rendering import ansi, text, xhtml, imtext
 from errbot.rendering.ansiext import enable_format, ANSI_CHRS, AnsiExtension
-from errbot.backends.base import Message, Person, Presence, ONLINE, OFFLINE, Room
+from errbot.backends.base import Message, Person, Presence, ONLINE, OFFLINE, Room, RoomOccupant
 from errbot.core import ErrBot
 from errbot.logs import console_hdlr
 
@@ -71,17 +73,152 @@ class TextPerson(Person):
 
     aclattr = person
 
-    def __unicode__(self):
-        if self.client:
-            return self._person + "/" + self._client
-        return self._person
-
-    __str__ = __unicode__
+    def __str__(self):
+        return '@' + self._person
 
     def __eq__(self, other):
         if not isinstance(other, Person):
             return False
         return self.person == other.person
+
+    def __hash__(self):
+        return self.person.__hash__()
+
+
+class TextRoom(Room):
+
+    def __init__(self, name, bot):
+        self._topic = ''
+        self._joined = False
+        self.name = name
+        self._bot = bot
+
+        # fill up the room with a coherent set of identities.
+        self._occupants = [TextOccupant('somebody', self),
+                           TextOccupant(TextPerson(bot.bot_config.BOT_ADMINS[0]), self),
+                           TextOccupant(bot.bot_identifier, self)]
+
+    def join(self, username=None, password=None):
+        self._joined = True
+
+    def leave(self, reason=None):
+        self._joined = False
+
+    def create(self):
+        self._joined = True
+
+    def destroy(self):
+        self._joined = False
+
+    @property
+    def exists(self):
+        return True
+
+    @property
+    def joined(self):
+        return self._joined
+
+    @property
+    def topic(self):
+        return self._topic
+
+    @topic.setter
+    def topic(self, topic):
+        self._topic = topic
+
+    @property
+    def occupants(self):
+        return self._occupants
+
+    def invite(self, *args):
+        pass
+
+    def __str__(self):
+        return '#' + self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+
+class TextOccupant(TextPerson, RoomOccupant):
+
+    def __init__(self, person, room):
+        super().__init__(person)
+        self._room = room
+
+    @property
+    def room(self):
+        return self._room
+
+    def __str__(self):
+        return '#%s/%s' % (self._room.name, self._person.person)
+
+    def __eq__(self, other):
+        return self.person == other.person and self.room == other.room
+
+    def __hash__(self):
+        return self.person.__hash__() + self.room.__hash__()
+
+
+class TextPlugin(BotPlugin):
+    """
+        Internal to TextBackend.
+    """
+
+    __errdoc__ = "Added commands for testing purposes"
+
+    @botcmd
+    def inroom(self, msg, args):
+        """
+           This puts you in a room with the bot.
+        """
+        self._bot._inroom = True
+        return 'Joined Room %s.' % self._bot._rooms[0]
+
+    @botcmd
+    def inperson(self, msg, args):
+        """
+           This puts you in a 1-1 chat with the bot.
+        """
+        self._bot._inroom = False
+        return 'Now in one-on-one with the bot.'
+
+    @botcmd
+    def asuser(self, msg, args):
+        """
+           This puts you in a room with the bot. You can specify a name otherwise it will default to 'luser'.
+        """
+        if args:
+            usr = args
+            if usr[0] != '@':
+                usr = '@' + usr
+            self._bot.user = self.build_identifier(usr)
+        else:
+            self._bot.user = self.build_identifier('@luser')
+        return 'You are now: %s' % self._bot.user
+
+    @botcmd
+    def asadmin(self, msg, args):
+        """
+           This puts you in a 1-1 chat with the bot.
+        """
+        self._bot.user = self.build_identifier(self.bot_config.BOT_ADMINS[0])
+        return 'You are now an admin: %s' % self._bot.user
+
+
+INTRO = """
+---
+You start as a **bot admin in a one-on-one conversation** with the bot.
+
+- Use `!inroom`{:color='blue'} to switch to a room conversation.
+- Use `!inperson`{:color='blue'} to switch back to a one-on-one conversation.
+- Use `!asuser`{:color='green'} to talk as a normal user.
+- Use `!asadmin`{:color='red'} to switch back as a bot admin.
+---
+"""
 
 
 class TextBackend(ErrBot):
@@ -97,9 +234,17 @@ class TextBackend(ErrBot):
             log.debug("Python readline module is not available")
             pass
 
-        self.bot_identifier = self.build_identifier('Err')
+        if 'username' in self.bot_config.BOT_IDENTITY:
+            self.bot_identifier = self.build_identifier(self.bot_config.BOT_IDENTITY['username'])
+        else:
+            # Just a default identity for the bot if nothing has been specified.
+            self.bot_identifier = self.build_identifier('@errbot')
+
+        log.debug('Bot username set at %s.', self.bot_identifier)
+        self._inroom = False
+        self._rooms = []
+
         self.demo_mode = self.bot_config.TEXT_DEMO_MODE if hasattr(self.bot_config, 'TEXT_DEMO_MODE') else False
-        self._rooms = set()
         if not self.demo_mode:
             self.md_html = xhtml()  # for more debug feedback on md
             self.md_text = text()  # for more debug feedback on md
@@ -111,8 +256,32 @@ class TextBackend(ErrBot):
         self.html_lexer = get_lexer_by_name("html", stripall=True)
         self.terminal_formatter = Terminal256Formatter(style='paraiso-dark')
         self.user = self.build_identifier(self.bot_config.BOT_ADMINS[0])
+        self._register_identifiers_pickling()
+
+    @staticmethod
+    def _unpickle_identifier(identifier_str):
+        return TextBackend.__build_identifier(identifier_str)
+
+    @staticmethod
+    def _pickle_identifier(identifier):
+        return TextBackend._unpickle_identifier, (str(identifier),)
+
+    def _register_identifiers_pickling(self):
+        """
+        Register identifiers pickling.
+        """
+        TextBackend.__build_identifier = self.build_identifier
+        for cls in (TextPerson, TextOccupant, TextRoom):
+            copyreg.pickle(cls, TextBackend._pickle_identifier, TextBackend._unpickle_identifier)
 
     def serve_forever(self):
+        # Add custom commands just for this backend.
+        self.inject_commands_from(TextPlugin(self, 'TextPlugin'))
+
+        if not self._rooms:
+            # artificially join a room if None were specified.
+            self.query_room('#testroom').join()
+
         if self.demo_mode:
             # disable the console logging once it is serving in demo mode.
             root = logging.getLogger()
@@ -120,15 +289,26 @@ class TextBackend(ErrBot):
             root.addHandler(logging.NullHandler())
         self.connect_callback()  # notify that the connection occured
         self.callback_presence(Presence(identifier=self.user, status=ONLINE))
+
+        self.send_message(Message(INTRO))
+
         try:
             while True:
-                if ANSI or self.demo_mode:
-                    entry = input('\n' + str(fg.cyan) + ' >>> ' + str(fx.reset))
+                if self._inroom:
+                    frm = TextOccupant(self.user, self.rooms[0])
+                    to = self.rooms[0]
                 else:
-                    entry = input('\n>>> ')
+                    frm = self.user
+                    to = self.bot_identifier
+
+                if ANSI or self.demo_mode:
+                    color = fg.red if self.user.person in self.bot_config.BOT_ADMINS[0] else fg.green
+                    entry = input('\n' + str(color) + '[%s ➡ %s]' % (frm, to) + str(fg.cyan) + ' >>> ' + str(fx.reset))
+                else:
+                    entry = input('\n[%s ➡ %s] >>> ' % (frm, to))
                 msg = Message(entry)
-                msg.frm = self.user
-                msg.to = self.bot_identifier
+                msg.frm = frm
+                msg.to = to
                 self.callback_message(msg)
 
                 mentioned = [self.build_identifier(word[1:]) for word in re.findall(r"@[\w']+", entry)
@@ -192,13 +372,22 @@ class TextBackend(ErrBot):
 
     def build_identifier(self, text_representation):
         if text_representation.startswith('#'):
-            return self.query_room(text_representation[1:])
-        return TextPerson(text_representation)
+            rem = text_representation[1:]
+            if '/' in text_representation:
+                room, person = rem.split('/')
+                return TextOccupant(TextPerson(person), TextRoom(room, self))
+            return self.query_room('#' + rem)
+        if not text_representation.startswith('@'):
+            raise ValueError('An identifier for the Text backend needs to start with # for a room or @ for a person.')
+        return TextPerson(text_representation[1:])
 
     def build_reply(self, msg, text=None, private=False):
         response = self.build_message(text)
         response.frm = self.bot_identifier
-        response.to = msg.frm
+        if private:
+            response.to = msg.frm
+        else:
+            response.to = msg.frm.room if isinstance(msg.frm, RoomOccupant) else msg.frm
         return response
 
     @property
@@ -206,58 +395,16 @@ class TextBackend(ErrBot):
         return 'text'
 
     def query_room(self, room):
-        text_room = TextRoom(room)
-        self._rooms.add(text_room)
+        if not room.startswith('#'):
+            raise ValueError('A Room name must start by #.')
+        text_room = TextRoom(room[1:], self)
+        if text_room not in self._rooms:
+            self._rooms.append(text_room)
         return text_room
 
+    @property
     def rooms(self):
         return self._rooms
 
     def prefix_groupchat_reply(self, message, identifier):
         message.body = '@{0} {1}'.format(identifier.nick, message.body)
-
-
-class TextRoom(Room):
-
-    def __init__(self, name):
-        self.topic_ = ''
-        self.joined_ = False
-        self.name = name
-
-    def join(self, username=None, password=None):
-        self.joined_ = True
-
-    def leave(self, reason=None):
-        self.joined_ = False
-
-    def create(self):
-        self.joined_ = True
-
-    def destroy(self):
-        self.joined_ = False
-
-    @property
-    def exists(self):
-        return True
-
-    @property
-    def joined(self):
-        return self.joined_
-
-    @property
-    def topic(self):
-        return self.topic_
-
-    @topic.setter
-    def topic(self, topic):
-        self.topic_ = topic
-
-    @property
-    def occupants(self):
-        return [self.build_identifier("Somebody")]
-
-    def invite(self, *args):
-        pass
-
-    def __str__(self):
-        return self.name

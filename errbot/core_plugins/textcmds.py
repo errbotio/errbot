2b38ec973629c5ad70960f97b99d6644d3b8a09b
Guillaume Binet <gbin@gootz.net>
2017-08-27 11:20:53 -0400

    Text mode is now persistent. (#1091)
    The Text mode now persist the context of the chat (multiline, admin,
    room etc...).
    
    Usually when I test a plugin I want to restart from the same state if I
    have to restart the bot. For example: I test a feature in a room or with
    a specific user I don't want to redo !inroom !asuser at every restart.
    
    This makes those commands persistents. It also cleanup the text backend
    code my promoting the fake plugin from Text into a core plugin in
    errbot.

---
+++ errbot/core_plugins/textcmds.py
@@ -0,0 +1,90 @@
+from errbot import BotPlugin, botcmd
+
+INROOM, USER, MULTILINE = 'inroom', 'user', 'multiline'
+
+
+class TextModeCmds(BotPlugin):
+    """
+        Internal to TextBackend.
+    """
+
+    __errdoc__ = "Added commands for testing purposes"
+
+    def activate(self):
+
+        # This won't activate the plugin in anything else than text mode.
+        if self.mode != 'text':
+            return
+
+        super().activate()
+
+        # Some defaults if it was never used before'.
+        if INROOM not in self:
+            self[INROOM] = False
+
+        if USER not in self:
+            self[USER] = self.build_identifier(self.bot_config.BOT_ADMINS[0])
+
+        if MULTILINE not in self:
+            self[MULTILINE] = False
+
+        # Restore the values to their live state.
+        self._bot._inroom = self[INROOM]
+        self._bot.user = self[USER]
+        self._bot._multiline = self[MULTILINE]
+
+    def deactivate(self):
+
+        # Save the live state.
+        self[INROOM] = self._bot._inroom
+        self[USER] = self._bot.user
+        self[MULTILINE] = self._bot._multiline
+
+        super().deactivate()
+
+    @botcmd
+    def inroom(self, msg, _):
+        """
+           This puts you in a room with the bot.
+        """
+        self._bot._inroom = True
+        return 'Joined Room %s.' % self._bot._rooms[0]
+
+    @botcmd
+    def inperson(self, msg, _):
+        """
+           This puts you in a 1-1 chat with the bot.
+        """
+        self._bot._inroom = False
+        return 'Now in one-on-one with the bot.'
+
+    @botcmd
+    def asuser(self, msg, args):
+        """
+           This puts you in a room with the bot. You can specify a name otherwise it will default to 'luser'.
+        """
+        if args:
+            usr = args
+            if usr[0] != '@':
+                usr = '@' + usr
+            self._bot.user = self.build_identifier(usr)
+        else:
+            self._bot.user = self.build_identifier('@luser')
+        return 'You are now: %s' % self._bot.user
+
+    @botcmd
+    def asadmin(self, msg, _):
+        """
+           This puts you in a 1-1 chat with the bot.
+        """
+        self._bot.user = self.build_identifier(self.bot_config.BOT_ADMINS[0])
+        return 'You are now an admin: %s' % self._bot.user
+
+    @botcmd
+    def ml(self, msg, _):
+        """
+           Switch back and forth between normal mode and multiline mode. Use this if you want to test
+           commands spanning multiple lines. Note: in multiline, press enter twice to end and send the message.
+        """
+        self._bot._multiline = not self._bot._multiline
+        return 'Multiline mode, press enter twice to end messages' if self._bot._multiline else 'Normal one line mode.'

b056c05762f2334f63192a9fe67885c955780f66
Guillaume Binet <gbin@gootz.net>
2017-09-24 09:37:27 -0400

    Trailing spaces.

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
                        'backends/styles/*.css',
                        'backends/images/*.svg',
                        'core_plugins/*.plug',
-                       'core_plugins/*.md',                       
+                       'core_plugins/*.md',
                        'core_plugins/templates/*.md',
                        'storage/*.plug',
                        'templates/initdir/example.py',

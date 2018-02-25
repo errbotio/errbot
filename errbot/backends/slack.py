1d709375ac7ddcb3f68f479f9427b6595fd6e8a9
Jeremy Kenyon <lanthos@gmail.com>
2018-02-24 23:37:18 -0800

    Added event handler to the Slack backend for the member_joined_channel event (reference https://api.slack.com/events/member_joined_channel).

--- errbot/backends/slack.py
+++ errbot/backends/slack.py
@@ -408,6 +408,7 @@ class SlackBackend(ErrBot):
             'hello': self._hello_event_handler,
             'presence_change': self._presence_change_event_handler,
             'message': self._message_event_handler,
+            'member_joined_channel': self._member_joined_channel_event_handler,
         }
 
         event_handler = event_handlers.get(event_type)
@@ -530,6 +531,10 @@ class SlackBackend(ErrBot):
 
         if mentioned:
             self.callback_mention(msg, mentioned)
+
+    def _member_joined_channel_event_handler(self, event):
+        """Event handler for the 'member_joined_channel' event"""
+        self.callback_room_joined(SlackRoom(channelid=event['channel'], bot=self))
 
     def userid_to_username(self, id_):
         """Convert a Slack user ID to their user name"""

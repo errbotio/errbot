98dbb6d201ac0d65bf76d97f3911e95b9eab7ba3
Carlos <nzlosh@yahoo.com>
2018-03-01 18:24:20 +0100

    Support breaking message body size into parts for Slack attachments. (#1118)
    Since the message may be posted in languages other than English, I think it'd be better to format the part as `[x/y]` rather than `[x of y]`.

--- errbot/backends/slack.py
+++ errbot/backends/slack.py
@@ -715,7 +715,6 @@ class SlackBackend(ErrBot):
             attachment['image_url'] = card.image
         if card.thumbnail:
             attachment['thumb_url'] = card.thumbnail
-        attachment['text'] = card.body
 
         if card.color:
             attachment['color'] = COLORS[card.color] if card.color in COLORS else card.color
@@ -723,20 +722,28 @@ class SlackBackend(ErrBot):
         if card.fields:
             attachment['fields'] = [{'title': key, 'value': value, 'short': True} for key, value in card.fields]
 
-        data = {
-            'text': ' ',
-            'channel': to_channel_id,
-            'attachments': json.dumps([attachment]),
-            'link_names': '1',
-            'as_user': 'true'
-        }
-        try:
-            log.debug('Sending data:\n%s', data)
-            self.api_call('chat.postMessage', data=data)
-        except Exception:
-            log.exception(
-                "An exception occurred while trying to send a card to %s.[%s]" % (to_humanreadable, card)
-            )
+        limit = min(self.bot_config.MESSAGE_SIZE_LIMIT, SLACK_MESSAGE_LIMIT)
+        parts = self.prepare_message_body(card.body, limit)
+        part_count = len(parts)
+        footer = attachment.get("footer", "")
+        for i in range(part_count):
+            if part_count > 1:
+                attachment["footer"] = "{} [{}/{}]".format(footer, i + 1, part_count)
+            attachment["text"] = parts[i]
+            data = {
+                'text': ' ',
+                'channel': to_channel_id,
+                'attachments': json.dumps([attachment]),
+                'link_names': '1',
+                'as_user': 'true'
+            }
+            try:
+                log.debug('Sending data:\n%s', data)
+                self.api_call('chat.postMessage', data=data)
+            except Exception:
+                log.exception(
+                    "An exception occurred while trying to send a card to %s.[%s]" % (to_humanreadable, card)
+                )
 
     def __hash__(self):
         return 0  # this is a singleton anyway

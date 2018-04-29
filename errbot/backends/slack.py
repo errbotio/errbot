63f0cf9353377549418f8deb47c3dc9f5a8e3268
Andrew Herrington <andrewthetechie@users.noreply.github.com>
2018-04-29 08:45:15 -0500

    Change to event.get for SlackRoomBot as well

--- errbot/backends/slack.py
+++ errbot/backends/slack.py
@@ -516,7 +516,7 @@ class SlackBackend(ErrBot):
             if subtype == "bot_message":
                 msg.frm = SlackRoomBot(
                     self.sc,
-                    bot_id=event['bot_id'],
+                    bot_id=event.get('bot_id'),
                     bot_username=event.get('username', ''),
                     channelid=event['channel'],
                     bot=self

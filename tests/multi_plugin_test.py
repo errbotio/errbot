229803624c0df9e1cbedbe0d8a0e135a8f251113
Guillaume Binet <gbinet@argo.ai>
2018-05-12 16:27:49 -0400

    This fixes the flakyness on multiplugin test.

--- tests/multi_plugin_test.py
+++ tests/multi_plugin_test.py
@@ -4,11 +4,12 @@ extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), 'multi_plugi
 
 # This tests the decorellation between plugin class names and real names
 # by making 2 instances of the same plugin collide on purpose.
+# We test 2 cases at the same time because it depends on the ordering of the loading.
 
 
 def test_first(testbot):
-    assert 'Multi1' == testbot.exec_command('!myname')
+    assert 'Multi1' == testbot.exec_command('!myname') or 'Multi1' == testbot.exec_command('!multi1-myname')
 
 
-def test_first(testbot):
-    assert 'Multi2' == testbot.exec_command('!multi2-myname')
+def test_other(testbot):
+    assert 'Multi2' == testbot.exec_command('!myname') or 'Multi2' == testbot.exec_command('!multi2-myname')

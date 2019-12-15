34c2212ae55b53534875da87c93cbc2430e4e97a
Guillaume Binet <gbin@gootz.net>
2019-12-15 09:48:17 -0500

    Fixed a deprecation warning for 3.9 on Mapping.

--- errbot/core.py
+++ errbot/core.py
@@ -19,7 +19,7 @@ import traceback
 from datetime import datetime
 from threading import RLock
 
-import collections
+from collections.abc import Mapping
 from multiprocessing.pool import ThreadPool
 
 from errbot import CommandError
@@ -412,7 +412,7 @@ class ErrBot(Backend, StoreMixin):
         # integrated templating
         # The template needs to be set and the answer from the user command needs to be a mapping
         # If not just convert the answer to string.
-        if template_name and isinstance(template_parameters, collections.Mapping):
+        if template_name and isinstance(template_parameters, Mapping):
             return tenv().get_template(template_name + '.md').render(**template_parameters)
 
         # Reply should be all text at this point (See https://github.com/errbotio/errbot/issues/96)

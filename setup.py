d91bcc5dc4d8101a114b1343e9c2fc3eae8ea0f7
Sijis Aviles <sijis.aviles+github@gmail.com>
2020-04-19 01:10:24 -0500

    Feature: Slack RTM Backend (#1416)
    * feat: Start slackclient rtm
    
    * slack clinet partially working
    
    * pass on slackclient-2
    
    * basic support.
    
    * fix: Ensure backend is registered as SlackRTM
    
    Co-authored-by: Guillaume Binet <gbin@gootz.net>

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ if __name__ == "__main__":
             'hipchat': ['hypchat', 'sleekxmpp', 'pyasn1', 'pyasn1-modules'],
             'IRC': ['irc', ],
             'slack': ['slackclient>=1.0.5,<2.0', ],
+            'slack-rtm': ['slackclient>=2.0', ],
             'telegram': ['python-telegram-bot', ],
             'XMPP': ['sleekxmpp', 'pyasn1', 'pyasn1-modules'],
             ':python_version<"3.7"': ['dataclasses'],  # backward compatibility for 3.3->3.6 for dataclasses

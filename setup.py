d58670e3ea1604cbd629869afa15470c64ea1b14
Sijis Aviles <sijis.aviles+github@gmail.com>
2020-04-19 02:04:56 -0500

    fix: Updating plugin via botcmd (#1413)
    * fix: Updating plugin via botcmd
    
    We need to track the master branch in git. This allows the botcmd to
    update the plugins as the references are configure properly.
    
    * chore: Use dulwich version with remote pulling

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ deps = ['webtest',
         'ansi',
         'Pygments>=2.0.2',
         'pygments-markdown-lexer>=0.1.0.dev39',  # sytax coloring to debug md
-        'dulwich'  # python implementation of git
+        'dulwich>=0.19.16'  # python implementation of git
         ]
 
 src_root = os.curdir

c02b5cb30385ddb7b3684cbcca3aba41ec5214c3
Sijis Aviles <sijis.aviles+github@gmail.com>
2022-07-03 02:51:43 -0500

    feat: add entrypoint plugin for backends

--- errbot/backend_plugin_manager.py
+++ errbot/backend_plugin_manager.py
@@ -5,7 +5,7 @@ from typing import Any, Iterator, List, Type, Union
 
 from errbot.plugin_info import PluginInfo
 
-from .utils import collect_roots
+from .utils import collect_roots, entry_point_plugins
 
 log = logging.getLogger(__name__)
 
@@ -44,7 +44,8 @@ class BackendPluginManager:
         self._base_class = base_class
 
         self.plugin_info = None
-        all_plugins_paths = collect_roots((base_search_dir, extra_search_dirs))
+        ep = entry_point_plugins(group="errbot.backend_plugins")
+        all_plugins_paths = collect_roots((base_search_dir, extra_search_dirs, ep))
 
         for potential_plugin in enumerate_backend_plugins(all_plugins_paths):
             if potential_plugin.name == plugin_name:
